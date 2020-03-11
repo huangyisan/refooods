25509cfc5d1992badf67247421ab5e456ce2bfd2
huangyisan <anonymousyisan@gmail.com>
2020-03-11 22:56:16 +0800

    加入debounce

--- pages/index/c-foodsList.vue
+++ pages/index/c-foodsList.vue
@@ -30,7 +30,7 @@
 						</view>
           <block v-for="(iten, indey) in foods[title]" :key="indey">
 						<view class="food-info-wrapper">
-						<img class="item-pic" :src="iten.img" alt="">
+						<img class="item-pic" :src="iten.img" alt="" lazy-load=true>
 						<view class="food-info">
             <text class="item-name">{{iten.name}}</text>
             <text class="item-des">{{iten.description.trim()}}</text>
